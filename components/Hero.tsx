56f2ececfe30dba197fe62d0f084d6baef699d26
kareemkemmo <111249325+kareemkemmo@users.noreply.github.com>
2024-12-31 23:52:14 +0000

    fixed-innerHTML

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
         <h1 className="text-4xl font-bold mb-4">Kareem Elhemaly</h1>
         <p className="text-xl mb-8">Computer Engineering Student at University of South Florida</p>
         <p className="mb-8 max-w-2xl mx-auto">
-        Specializing in full-stack development, algorithm design, and embedded systems. Whether I'm building advanced data structures or designing intuitive user interfaces, my work is driven by creativity, precision, and a desire to make a lasting impact.
+        Specializing in full-stack development, algorithm design, and embedded systems. Whether I am building advanced data structures or designing intuitive user interfaces, my work is driven by creativity, precision, and a desire to make a lasting impact.
         </p>
         <Button asChild>
           <a href="#contact">Get in Touch</a>
