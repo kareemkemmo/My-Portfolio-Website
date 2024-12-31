4df9a9c850a69ddd6afe0602d1936edd0789efef
kareemkemmo <111249325+kareemkemmo@users.noreply.github.com>
2024-12-31 20:36:49 +0000

    fixes

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 items-center">
+      <div className="container flex h-14 items-center max-w-full px-3.5">
         <nav className="flex flex-1 items-center justify-between">
           <a href="#" className="text-lg font-semibold">Kareem Elhemaly</a>
           <ul className="flex space-x-4">
